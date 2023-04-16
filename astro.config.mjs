a94afd6f38bcff89314d87f5f5b9c72cd2af551e
Robbie Prokop <robbieprokop@gmail.com>
2023-04-16 17:52:10 -0600

    add sitemap and yandex verification

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://zizikarma.com",
+  integrations: [
+    react(),
+    sitemap({
+      filter: (page) => page !== "https://zizikarma.com/test-blog-post/",
+    }),
+  ],
 });
